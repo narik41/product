6c4920aee5e80ac977bd8289cd6cfdb66ea27dca
Kiran Subedi <kiran.subedi@treeleaf.ai>
2022-08-06 07:47:41 +0545

    WIP : create product form

---
+++ src/main/java/com/upwork/product/product/ProductController.java
@@ -0,0 +1,17 @@
+package com.upwork.product.product;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/product")
+@Controller
+public class ProductController {
+
+    @GetMapping("/create")
+    public ModelAndView create() {
+        ModelAndView modelAndView = new ModelAndView("product/create.html");
+        return modelAndView;
+    }
+}
